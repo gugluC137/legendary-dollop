469b26d45e4bfa814d6b10b2837c78889b2a4f83
BiswaBehera <behera.biswa5398@gmail.com>
2022-05-11 01:17:57 +0530

    next permutation

--- src/com/biswa/dsa/code/array/SetMatrixZeroes.java
+++ src/com/biswa/dsa/code/array/SetMatrixZeroes.java
@@ -34,4 +34,29 @@ public class SetMatrixZeroes {
 
   }
 
+  public void solveOptimised(int[][] matrix) {
+    int rowLength = matrix.length;
+    int columnLength = matrix[0].length;
+    int col0 = 1;
+
+    for (int i = 0; i < rowLength; i++) {
+      if (matrix[i][0] == 0) col0 = 0;
+      for (int j = 1; j < columnLength; j++) {
+        if (matrix[i][j] == 0) {
+          matrix[i][0] = matrix[0][j] = 0;
+        }
+      }
+    }
+
+    for (int i = rowLength-1; i >= 0; i--) {
+      for (int j = columnLength-1; j > 0; j--) {
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+          matrix[i][j] = 0;
+        }
+      }
+      if (col0 == 0) matrix[i][0] = 0;
+    }
+
+  }
+
 }
---
+++ src/com/biswa/dsa/util/GenericUtils.java
@@ -0,0 +1,18 @@
+package com.biswa.dsa.util;
+
+import java.util.List;
+
+public class GenericUtils {
+
+  public static void swap(List<Integer> list, int pos1, int pos2) {
+    int temp = list.get(pos1);
+    list.set(pos1, list.get(pos2));
+    list.set(pos2, temp);
+  }
+  public static void swap(int[] list, int pos1, int pos2) {
+    int temp = list[pos1];
+    list[pos1] = list[pos2];
+    list[pos2] = temp;
+  }
+
+}
