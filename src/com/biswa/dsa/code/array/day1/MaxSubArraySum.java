41b795f964e4f9b6978a221e845d4f2ba9882ff7
Biswajit Behera <behera.biswa5398@gmail.com>
2022-06-05 18:12:44 +0530

    Arrays day1 (#1)
    * first commit for arrays
    
    * next permutation
    
    * array day-1 more sols
    
    * array day1 complete

---
+++ src/com/biswa/dsa/code/array/day1/MaxSubArraySum.java
@@ -0,0 +1,37 @@
+package com.biswa.dsa.code.array.day1;
+
+import static com.biswa.dsa.util.CustomReader.*;
+
+import java.io.IOException;
+
+public class MaxSubArraySum {
+//Kadane's algorithm
+    public static long maxSubArraySum(int[] arr) {
+        int sum = 0, maxSum = arr[0];
+        for (int j : arr) {
+            sum += j;
+            if (maxSum < sum) maxSum = sum;
+            if (sum < 0) sum = 0;
+        }
+        return maxSum;
+
+//        for (int i = 0; i < n; i++){
+//            sum += arr[i];
+//            if (maxSum < sum) maxSum = sum;
+//            if (sum < 0) sum = 0;
+//        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        init();
+        int n = nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = nextInt();
+        }
+        System.out.println(maxSubArraySum(arr));
+
+    }
+
+}
