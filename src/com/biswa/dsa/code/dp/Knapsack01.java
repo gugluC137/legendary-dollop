0cb216cf4a66dab4a0b50dd5ffd3d3772c947887
gugluC137 <behera.biswa5398@gmail.com>
2023-07-17 01:00:24 +0530

    understood knapsack

---
+++ src/com/biswa/dsa/code/dp/Knapsack01.java
@@ -0,0 +1,111 @@
+package com.biswa.dsa.code.dp;
+
+public class Knapsack01 {
+
+    public int solWithRecursion(int[] weight, int[] value, int n, int maxWeight) {
+        if (maxWeight == 0) return 0;
+        if (n == 0) {
+            if (maxWeight >= weight[n]) return value[n];
+            else return 0;
+        }
+
+        int pick = 0;
+        if (maxWeight >= weight[n]) {
+            pick = value[n] + solWithRecursion(weight, value, n-1, maxWeight-weight[n]);
+        }
+        int notPick = solWithRecursion(weight, value, n-1, maxWeight);
+
+        return Math.max(pick, notPick);
+    }
+
+    public int solWithMemoization(int[] weight, int[] value, int n, int maxWeight, int[][] dp) {
+        if (maxWeight == 0) return 0;
+        if (n == 0) {
+            if (maxWeight >= weight[n]) return value[n];
+            else return 0;
+        }
+
+        if (dp[n][maxWeight] != -1) return dp[n][maxWeight];
+
+        int pick = 0;
+        if (maxWeight >= weight[n]) {
+            pick = value[n] + solWithRecursion(weight, value, n-1, maxWeight-weight[n]);
+        }
+        int notPick = solWithRecursion(weight, value, n-1, maxWeight);
+
+        return dp[n][maxWeight] = Math.max(pick, notPick);
+    }
+
+    public int solWithTabulation(int[] weight, int[] value, int n, int maxWeight) {
+        int[][] dp = new int[n][maxWeight+1];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 1; i < maxWeight+1; i++) {
+            dp[0][i] = i >= weight[0] ? value[0] : 0;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < maxWeight+1; j++) {
+                int notPick = dp[i-1][j];
+                int pick = 0;
+                if (j >= weight[i]) {
+                    pick = value[i] + dp[i-1][j-weight[i]];
+                }
+
+                dp[i][j] = Math.max(pick, notPick);
+            }
+        }
+
+        return dp[n-1][maxWeight];
+    }
+
+    public int solWithSpaceOptimisation(int[] weight, int[] value, int n, int maxWeight) {
+        int[] prev = new int[maxWeight+1];
+        prev[0] = 0;
+        for (int i = 1; i < maxWeight+1; i++) {
+            prev[i] = i >= weight[0] ? value[0] : 0;
+        }
+
+        for (int i = 1; i < n; i++) {
+            int[] cur = new int[maxWeight+1];
+            cur[0] = 0;
+            for (int j = 1; j < maxWeight+1; j++) {
+                int notPick = prev[j];
+                int pick = 0;
+                if (j >= weight[i]) {
+                    pick = value[i] + prev[j-weight[i]];
+                }
+
+                cur[j] = Math.max(pick, notPick);
+            }
+            prev = cur;
+        }
+
+        return prev[maxWeight];
+    }
+
+    public int solWithSpaceOptimisationI(int[] weight, int[] value, int n, int maxWeight) {
+        int[] dp = new int[maxWeight+1];
+        dp[0] = 0;
+        for (int i = 1; i < maxWeight+1; i++) {
+            dp[i] = i >= weight[0] ? value[0] : 0;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = maxWeight; j >= 0; j--) {
+                int notPick = dp[j];
+                int pick = 0;
+                if (j >= weight[i]) {
+                    pick = value[i] + dp[j - weight[i]];
+                }
+
+                dp[j] = Math.max(pick, notPick);
+            }
+        }
+
+        return dp[maxWeight];
+    }
+
+}
---
+++ src/com/biswa/dsa/code/dp/SubestSumK.java
@@ -0,0 +1,89 @@
+package com.biswa.dsa.code.dp;
+
+//https://www.codingninjas.com/studio/problems/1550954
+public class SubestSumK {
+
+    public boolean solWithRecursion(int n, int k, int[] arr) {
+        if (k == 0) return true;
+        if (n == 0) return arr[n] == k;
+
+        boolean pick = false;
+        if (k >= arr[n]) {
+            pick = solWithRecursion(n-1, k - arr[n], arr);
+        }
+        boolean notPick = solWithRecursion(n-1, k, arr);
+
+        return pick || notPick;
+    }
+
+    public boolean solWithMemoization(int n, int k, int[] arr, int[][] dp) {
+        if (k == 0) return true;
+        if (n == 0) return arr[n] == k;
+
+        if (dp[n][k] != -1) {
+            if (dp[n][k] == 0) return false;
+            else return true;
+        }
+
+        boolean pick = false;
+        if (k >= arr[n]) {
+            pick = solWithMemoization(n-1, k - arr[n], arr, dp);
+        }
+        boolean notPick = solWithMemoization(n-1, k, arr, dp);
+
+        if (pick || notPick) dp[n][k] = 1;
+        else dp[n][k] = 0;
+
+        return pick || notPick;
+    }
+
+    public boolean solWithTabulation(int n, int k, int[] arr) {
+        boolean[][] dp = new boolean[n][k+1];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k+1; j++) {
+                if (j == 0) {
+                    dp[i][j] = true;
+                } else if (i == 0) {
+                    dp[i][j] = arr[i] == j;
+                } else {
+                    boolean pick = false;
+                    if (j >= arr[i]) {
+                        pick = dp[i-1][j - arr[i]];
+                    }
+                    boolean notPick = dp[i-1][j];
+
+                    dp[i][j] = pick || notPick;
+                }
+            }
+        }
+
+        return dp[n-1][k];
+    }
+
+    public boolean solWithSpaceOptimisation(int n, int k, int[] arr) {
+        boolean[] prev = new boolean[k+1];
+        prev[0] = true;
+        for (int i = 1; i <= k; i++) {
+            prev[i] = arr[0] == i;
+        }
+
+        for (int i = 1; i < n; i++) {
+            boolean[] cur = new boolean[k+1];
+            cur[0] = true;
+            for (int j = 1; j <= k; j++) {
+                boolean pick = false;
+                if (j >= arr[i]) {
+                    pick = prev[j - arr[i]];
+                }
+                boolean notPick = prev[j];
+
+                cur[j] = pick || notPick;
+            }
+            prev = cur;
+        }
+
+        return prev[k];
+    }
+
+}
